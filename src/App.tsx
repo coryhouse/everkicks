c08290fc44c63118975507e3c72dd2492286cd0b
Cory House <housecor@gmail.com>
2022-05-09 13:14:35 -0500

    Add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import { useState } from "react";
 
 export default function App() {
+  // Declare stuff in state that changes over time.
+  // That way React knows to re-render when the state changes
   const [shoes, setShoes] = useState(["Nike Air Max", "Reebok Pump"]);
+  const [shoe, setShoe] = useState("");
 
+  // Exercise 1: Render "Add shoe" button above the list of shoes.
   return (
     <>
       <h1>Everkicks</h1>
+      <form
+        onSubmit={() => {
+          //setShoes([...shoes, ????])
+        }}
+      >
+        <label htmlFor="shoe-name">Shoe name</label>
+        <br />
+        <input
+          type="text"
+          id="shoe-name"
+          value={shoe}
+          onChange={(event) => setShoe(event.target.value)}
+        />
+        <button>Add Shoe</button>
+      </form>
       <ul>
         {shoes.map((shoe) => (
           <li>{shoe}</li>
