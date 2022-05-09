05f7108307c8f01ac64779eeb6f58f0862d5ab45
Cory House <housecor@gmail.com>
2022-05-09 16:51:57 -0500

    Improve markup and add nav

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,33 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import ManageShoes from "./ManageShoes";
 
+// Exercise 3: Implement nav bar with links to both pages.
 export default function App() {
   const [shoes, setShoes] = useState(["Nike Air Max", "Reebok Pump"]);
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home shoes={shoes} />} />
-        <Route
-          path="/admin/shoes"
-          element={<ManageShoes shoes={shoes} setShoes={setShoes} />}
-        />
-      </Routes>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/admin/shoes">Manage Shoes</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home shoes={shoes} />} />
+          <Route
+            path="/admin/shoes"
+            element={<ManageShoes shoes={shoes} setShoes={setShoes} />}
+          />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
