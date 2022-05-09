a30e76048481d15615b491b155b598fdd36c89f1
Cory House <housecor@gmail.com>
2022-05-09 17:29:18 -0500

    config cypress eslint

--- cypress/.eslintrc.js
+++ cypress/.eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
+  env: {
+    "cypress/globals": true,
+  },
+  plugins: ["cypress"],
+  extends: ["plugin:cypress/recommended"],
   rules: {
     "testing-library/await-async-query": "off",
     "testing-library/prefer-screen-queries": "off",
