95c10f368536b116e6561fcf0466cf6a68168b70
Cory House <housecor@gmail.com>
2022-05-09 20:56:39 -0500

    Add test

--- cypress/integration/ManageShoes.spec.ts
+++ cypress/integration/ManageShoes.spec.ts
@@ -3,5 +3,17 @@
 describe("ManageShoes", () => {
   it("should support adding a shoe and displaying the shoe on the homepage", () => {
     cy.visit("http://localhost:3000/admin/shoes");
+    cy.findByLabelText("Shoe name").type("British Knights");
+    cy.findByRole("button", { name: "Add Shoe" }).click();
+    cy.findByText("British Knights");
+
+    //Now the input should be empty because we just submitted the form
+    cy.findByLabelText("Shoe name").should("be.empty");
+    cy.findByRole("navigation").findByRole("link", { name: "Home" }).click();
+
+    // We've moved to a new page, so assure we're on the new URL
+    cy.url().should("eq", "http://localhost:3000/");
+
+    cy.findByText("British Knights");
   });
 });
