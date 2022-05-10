0780dba26e7826f11d17d5d238daaf71c820dd1d
Cory House <housecor@gmail.com>
2022-05-10 17:29:03 -0500

    It's better now

--- src/reusable/TextInput.tsx
+++ src/reusable/TextInput.tsx
@@ -1,11 +1,34 @@
-import { ChangeEventHandler } from "react";
+import { ChangeEventHandler, FocusEventHandler } from "react";
 
 type TextInputProps = {
-  value: string;
+  /** Text input value */
+  value: string | number;
+
+  /** Validation error */
   error?: string;
+
+  /** Text input label */
   label: string;
+
+  /** Optionally specify the step for an input with a type of number */
+  step?: string;
+
+  /** Text input's **HTML** ID
+   * List of items
+   * - one
+   * - two
+   *
+   * [See docs here](http://google.com)
+   */
   id: string;
+
+  /** Text input's onChange handler */
   onChange: ChangeEventHandler<HTMLInputElement>;
+
+  /** Text input's onBlur handler */
+  onBlur?: FocusEventHandler<HTMLInputElement>;
+
+  /** Text input's type */
   type?: "text" | "date" | "number" | "phone" | "password";
 };
 
@@ -14,15 +37,39 @@ export default function TextInput({
   label,
   error,
   onChange,
+  onBlur,
   id,
   type = "text",
+  step,
 }: TextInputProps) {
+  // Dynamically building props so that we only apply the step property when relevant.
+  const inputProps: any = {
+    type,
+    id,
+    value,
+    onChange,
+    onBlur,
+  };
+
+  if (type === "number") {
+    inputProps.step = step;
+  }
+
+  if (error) {
+    inputProps["aria-invalid"] = true;
+    inputProps["aria-describedby"] = id + "-error";
+  }
+
   return (
     <div>
       <label htmlFor={id}>{label}</label>
       <br />
-      <input type={type} id={id} value={value} onChange={onChange} />
-      {error && <div>{error}</div>}
+      <input {...inputProps} />
+      {error && (
+        <p id={id + "-error"} role="alert">
+          {error}
+        </p>
+      )}
     </div>
   );
 }
