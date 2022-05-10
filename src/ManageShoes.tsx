253a267d7c9f939682ef16e25e0d4c088aac3170
Cory House <housecor@gmail.com>
2022-05-10 11:55:47 -0500

    Use object instead of string

--- src/ManageShoes.tsx
+++ src/ManageShoes.tsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
+import { Shoe } from "./types/types";
 
 interface ManageShoesProps {
-  shoes: string[];
-  setShoes: (shoes: string[]) => void;
+  shoes: Shoe[];
+  setShoes: (shoes: Shoe[]) => void;
 }
+
+// Declaring outside the component so that it is only allocated once
+// Note: default values only apply once, but are parsed on every render.
+const newShoe: Shoe = { name: "", brand: "" };
 
 export default function ManageShoes({ shoes, setShoes }: ManageShoesProps) {
   // Declare stuff in state that changes over time.
   // That way React knows to re-render when the state changes
-  const [shoe, setShoe] = useState("");
+  const [shoe, setShoe] = useState<Shoe>(newShoe);
+
+  const { name, brand } = shoe;
+
+  function onChange(
+    event:
+      | React.ChangeEvent<HTMLSelectElement>
+      | React.ChangeEvent<HTMLInputElement>
+  ) {
+    // Copy the shoe object, then set the brand property
+    setShoe({ ...shoe, [event.target.id]: event.target.value });
+  }
 
   return (
     <>
@@ -19,18 +35,26 @@ export default function ManageShoes({ shoes, setShoes }: ManageShoesProps) {
         <form
           onSubmit={(event) => {
             event.preventDefault(); // Hey browser, don't post back
-            setShoes([...shoes, shoe]);
-            setShoe("");
+            // setShoes([...shoes, { name: shoe, brand: brand }]);
+            // setShoe("");
           }}
         >
-          <label htmlFor="shoe-name">Shoe name</label>
-          <br />
-          <input
-            type="text"
-            id="shoe-name"
-            value={shoe}
-            onChange={(event) => setShoe(event.target.value)}
-          />
+          <div>
+            <label htmlFor="brand">Brand</label>
+            <br />
+            <select id="brand" value={brand ?? ""} onChange={onChange}>
+              <option value="">Select brand</option>
+              <option value="Nike">Nike</option>
+              <option value="Adidas">Adidas</option>
+              <option value="British Knights">British Knights</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="name">Shoe name</label>
+            <br />
+            <input type="text" id="name" value={name} onChange={onChange} />
+          </div>
           <button type="submit">Add Shoe</button>
         </form>
       </section>
@@ -39,7 +63,7 @@ export default function ManageShoes({ shoes, setShoes }: ManageShoesProps) {
         <h2>Shoes</h2>
         <ul>
           {shoes.map((shoe) => (
-            <li>{shoe}</li>
+            <li>{shoe.name}</li>
           ))}
         </ul>
       </section>
