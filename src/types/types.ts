1e10fd5e2579b9e4df6f430ac6211a6457fc3cc8
Cory House <housecor@gmail.com>
2022-05-10 13:53:48 -0500

    Add new properties

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,9 @@
 export type Shoe = {
   brand: string;
   name: string;
+  // Exercise 1: Add these fields to ManageShoe.
+  // Display them on both ManageShoe and Home.
+  size: number;
+  price: number;
+  releaseDate: string;
 };
