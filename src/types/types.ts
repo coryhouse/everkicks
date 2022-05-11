d7a21ab63b9bde140939826d9802f757d0f88f66
Cory House <housecor@gmail.com>
2022-05-11 13:49:32 -0500

    Enhance types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,11 @@
-export type Shoe = {
+export interface NewShoe {
   brand: string;
   name: string;
   size: number;
   price: number;
   releaseDate: string;
-};
+}
+
+export interface Shoe extends NewShoe {
+  id: number;
+}
