e6f77d8797ebfc27d83dfde3105cbd3f7f85aa77
Cory House <housecor@gmail.com>
2022-05-11 13:49:51 -0500

    Display shoe in card

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,18 +7,35 @@ export default function Home() {
 
   useEffect(() => {
     async function fetch() {
-      const shoes = await getShoes();
+      const shoesResp = await getShoes();
+      setShoes(shoesResp);
     }
     fetch();
-  });
+    // Dependency array specifies when this should re-run.
+    // Since it should only run once, we declare an empty array.
+  }, []);
 
   return (
     <>
       <h1>Everkicks</h1>
 
-      <ul>
+      <ul style={{ listStyleType: "none", display: "flex", padding: 0 }}>
         {shoes.map((shoe) => (
-          <li>{JSON.stringify(shoe)}</li>
+          <li>
+            <article style={{ border: "solid 1px gray", padding: 16 }}>
+              <h2>
+                {shoe.brand} {shoe.name}
+              </h2>
+              <p>Size: {shoe.size}</p>
+              <p>
+                Released{" "}
+                <time dateTime={shoe.releaseDate}>{shoe.releaseDate}</time>
+              </p>
+              <p style={{ fontWeight: "bold", color: "green" }}>
+                ${shoe.price}
+              </p>
+            </article>
+          </li>
         ))}
       </ul>
     </>
