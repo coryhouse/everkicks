70abde8aa69e58ec23885368cbf8ebf44bed7d7f
Cory House <housecor@gmail.com>
2022-05-12 17:01:57 -0500

    Move handlers inline and read config

--- src/useWorker.tsx
+++ src/useWorker.tsx
@@ -1,11 +1,38 @@
-import { setupWorker, SetupWorkerApi } from "msw";
+import { rest, setupWorker, SetupWorkerApi } from "msw";
 import { useEffect, useState } from "react";
-import { handlers } from "../src/mocks/handlers";
+import { GetShoesResponse } from "./DevTools";
 
-export function useWorker() {
+type UseWorkerConfig = {
+  getShoesResponse: GetShoesResponse;
+};
+
+export function useWorker(config: UseWorkerConfig) {
   const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
+    const handlers = [
+      rest.get("http://localhost:3001/shoes", (req, res, ctx) => {
+        if (config.getShoesResponse === "success") {
+          return res(
+            ctx.status(200),
+            ctx.json([
+              {
+                brand: "Nike",
+                name: "Mock One",
+                price: "95",
+                releaseDate: "1998-01-01",
+                size: "07",
+                id: 1,
+              },
+            ])
+          );
+        } else {
+          return res(ctx.status(500));
+        }
+      }),
+      //rest.delete("/shoes",
+    ];
+
     // This configures a Service Worker with the given request handlers.
     const worker = setupWorker(...handlers);
 
@@ -20,7 +47,7 @@ export function useWorker() {
     return () => {
       worker.stop();
     };
-  }, []);
+  }, [config.getShoesResponse]);
 
   return isReady;
 }
