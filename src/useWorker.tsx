54747a44e030d8f16d809008faea9556a34c6a89
Cory House <housecor@gmail.com>
2022-05-12 16:28:49 -0500

    Add hook

--- src/useWorker.tsx
+++ src/useWorker.tsx
@@ -1,17 +1,26 @@
-import { setupWorker } from "msw";
-import { useEffect } from "react";
+import { setupWorker, SetupWorkerApi } from "msw";
+import { useEffect, useState } from "react";
 import { handlers } from "../src/mocks/handlers";
 
 export function useWorker() {
+  const [isReady, setIsReady] = useState(false);
+
   useEffect(() => {
     // This configures a Service Worker with the given request handlers.
     const worker = setupWorker(...handlers);
 
-    worker.start();
+    const startWorker = async (worker: SetupWorkerApi) => {
+      await worker.start();
+      setIsReady(true);
+    };
+
+    startWorker(worker);
 
     // This is run on unmount
     return () => {
       worker.stop();
     };
   }, []);
+
+  return isReady;
 }
