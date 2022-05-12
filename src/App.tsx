3fbb2a1a7d326a75c2575ecdad006f54c94e6a02
Cory House <housecor@gmail.com>
2022-05-12 17:55:56 -0500

    add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { lazy, Suspense, useEffect, useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { getShoes } from "./api/shoeApi";
+import ErrorFallback from "./ErrorFallback";
 import Spinner from "./Spinner";
 import { Shoe } from "./types/types";
 import { useUserContext } from "./UserContext";
@@ -11,18 +13,25 @@ const ManageShoes = lazy(() => import("./ManageShoes"));
 export default function App() {
   const [shoes, setShoes] = useState<Shoe[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
   const { user } = useUserContext();
 
   useEffect(() => {
     async function fetch() {
-      const shoesResp = await getShoes();
-      setShoes(shoesResp);
-      setIsLoading(false);
+      try {
+        const shoesResp = await getShoes();
+        setShoes(shoesResp);
+        setIsLoading(false);
+      } catch (e) {
+        setError(e as Error);
+      }
     }
     fetch();
     // Dependency array specifies when this should re-run.
     // Since it should only run once, we declare an empty array.
   }, []);
+
+  if (error) throw error;
 
   return (
     <BrowserRouter>
@@ -44,15 +53,25 @@ export default function App() {
       <main>
         <Suspense fallback={<Spinner />}>
           <Routes>
-            <Route path="/" element={<Home shoes={shoes} />} />
+            <Route
+              path="/"
+              element={
+                <ErrorBoundary FallbackComponent={ErrorFallback}>
+                  <Home shoes={shoes} />
+                </ErrorBoundary>
+              }
+            />
+
             <Route
               path="/admin/shoes"
               element={
-                <ManageShoes
-                  isLoading={isLoading}
-                  shoes={shoes}
-                  setShoes={setShoes}
-                />
+                <ErrorBoundary FallbackComponent={ErrorFallback}>
+                  <ManageShoes
+                    isLoading={isLoading}
+                    shoes={shoes}
+                    setShoes={setShoes}
+                  />
+                </ErrorBoundary>
               }
             />
           </Routes>
--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,18 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { UserContextProvider } from "./UserContext";
 import App from "./App";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ErrorFallback";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <UserContextProvider>
-    <ToastContainer />
-    {process.env.REACT_APP_SHOW_DEV_TOOLS === "Y" ? <AppDevTools /> : <App />}
-  </UserContextProvider>
+  <ErrorBoundary FallbackComponent={ErrorFallback}>
+    <UserContextProvider>
+      <ToastContainer />
+      {process.env.REACT_APP_SHOW_DEV_TOOLS === "Y" ? <AppDevTools /> : <App />}
+    </UserContextProvider>
+  </ErrorBoundary>
 );
