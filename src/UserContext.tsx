fc34e0a466315b82ad055a3e720d3c0ec34f85e8
Cory House <housecor@gmail.com>
2022-05-12 13:48:05 -0500

    Add context

---
+++ src/UserContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useState } from "react";
+import { User } from "./types/types";
+
+type UserContextValue = {
+  user: User;
+  setUser: (user: User) => void;
+};
+
+type UserContextProviderProps = {
+  children: React.ReactNode;
+};
+
+const UserContext = createContext<UserContextValue | null>(null);
+
+export function UserContextProvider({ children }: UserContextProviderProps) {
+  const [user, setUser] = useState<User>("customer");
+
+  // These are the values we want to expose via context.
+  const contextValue = {
+    user,
+    setUser,
+  };
+
+  return (
+    <UserContext.Provider value={contextValue}>{children}</UserContext.Provider>
+  );
+}
+
+export function useUserContext() {
+  const userContext = useContext(UserContext);
+
+  if (userContext === null) {
+    throw new Error(
+      "useUserContext must be called from a component that's a child of UserContextProvider"
+    );
+  }
+
+  return userContext;
+}
