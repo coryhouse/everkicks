c805cd4c6d46a5a0a599d2e124e4d50a0077bcfb
Cory House <housecor@gmail.com>
2022-05-12 11:30:19 -0500

    Enhance tests

--- src/ManageShoes.test.tsx
+++ src/ManageShoes.test.tsx
@@ -2,7 +2,30 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import ManageShoes from "./ManageShoes";
 
-it("renders heading", () => {
-  render(<ManageShoes shoes={[]} setShoes={() => {}} />);
-  screen.getByRole("heading", { name: "Everkicks: Manage Shoes" });
+it("renders the h1 heading and spinner when isLoading is true", () => {
+  render(<ManageShoes isLoading shoes={[]} setShoes={() => {}} />);
+  screen.getByRole("heading", { name: "Manage Shoes" });
+  screen.getByLabelText("Loading...");
+});
+
+it("renders the h2 and list and hiding the spinner when isLoading is false", () => {
+  render(
+    <ManageShoes
+      isLoading={false}
+      shoes={[
+        {
+          brand: "Brand",
+          id: 1,
+          name: "Name",
+          price: 0,
+          releaseDate: "2020-01-01",
+          size: 7,
+        },
+      ]}
+      setShoes={() => {}}
+    />
+  );
+  screen.getByRole("heading", { name: "Manage Shoes" });
+  screen.getByRole("heading", { name: "Brand Name" });
+  expect(screen.queryByLabelText("Loading...")).not.toBeInTheDocument();
 });
