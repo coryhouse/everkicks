92bf2bdcf642327330f4e76182bf11bea261326d
Cory House <housecor@gmail.com>
2022-05-13 15:47:56 -0500

    Add

---
+++ src/ExpensiveTree.tsx
@@ -0,0 +1,7 @@
+export default function ExpensiveTree() {
+  let now = performance.now();
+  while (performance.now() - now < 100) {
+    // Artificial delay -- do nothing for 100ms
+  }
+  return <p>I am a very slow component tree.</p>;
+}
