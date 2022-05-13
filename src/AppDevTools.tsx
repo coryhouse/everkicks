cba079e23ef14bc166e18eed2c1db3cb4ffdea09
Cory House <housecor@gmail.com>
2022-05-13 11:28:50 -0500

    Implement localStorage and wrap app in error boundary

--- src/AppDevTools.tsx
+++ src/AppDevTools.tsx
@@ -1,23 +1,36 @@
-import { useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import App from "./App";
 import DevTools, { GetShoesResponse } from "./DevTools";
+import { useLocalStorage } from "./useLocalStorage";
 import { useWorker } from "./useWorker";
 
-export function AppDevTools() {
-  const [getShoesResponse, setGetShoesResponse] =
-    useState<GetShoesResponse>("500");
+export type DevToolsConfig = {
+  getShoesResponse: GetShoesResponse;
+};
 
-  const isReady = useWorker({
-    getShoesResponse,
-  });
+const defaultDevToolsConfig: DevToolsConfig = {
+  getShoesResponse: "success",
+};
+
+export function AppDevTools() {
+  const [config, setConfig] = useLocalStorage(
+    "devtools",
+    defaultDevToolsConfig
+  );
+
+  const isReady = useWorker(config);
 
   return isReady ? (
     <>
-      <App />
-      <DevTools
-        getShoesResponse={getShoesResponse}
-        setGetShoesResponse={setGetShoesResponse}
-      />
+      <ErrorBoundary
+        // Throw away the old app and re-render the App and all children
+        // anytime the dev tools config changes.
+        key={JSON.stringify(config)}
+        fallback={<>App crashed. :(</>}
+      >
+        <App />
+      </ErrorBoundary>
+      <DevTools config={config} setConfig={setConfig} />
     </>
   ) : null;
 }
