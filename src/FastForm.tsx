5a75379552e83f000ad0622f87bfa4d341410f98
Cory House <housecor@gmail.com>
2022-05-14 15:15:05 -0500

    Finish fast form

--- src/FastForm.tsx
+++ src/FastForm.tsx
@@ -1,39 +1,80 @@
 import { useState } from "react";
 import FastInput from "./FastInput";
 
-type FormStatus = "Idle" | "Submitted";
+export type FormStatus = "Idle" | "Submitted";
+export type FieldName = "firstName" | "email";
+export type ValidateFunction = (name: FieldName, value: string) => string;
 
 type FastFormProps = {
   slowComponent: React.ReactNode;
 };
 
+/**
+ * The FastForm component takes the uncontrolled approach. Rather than keeping
+ * track of all the values and passing the values to each field, we let the
+ * fields keep track of things themselves and we retrieve the values from the
+ * form.elements when it's submitted.
+ */
 export default function FastForm({ slowComponent }: FastFormProps) {
   const [formStatus, setFormStatus] = useState<FormStatus>("Idle");
   const [submitCount, setSubmitCount] = useState(0);
-  function required(value: string) {
-    return !value ? "This is required." : "";
+
+  function validateRequired(value: string) {
+    return value ? "" : "Required field.";
+  }
+
+  function validateEmail(value: string) {
+    return value.includes("@") ? "" : "Invalid email.";
+  }
+
+  // Validate the provided field/value combination
+  const validate: ValidateFunction = (fieldName: FieldName, value: string) => {
+    switch (fieldName) {
+      case "firstName":
+        return validateRequired(value);
+      case "email":
+        return validateEmail(value);
+    }
+  };
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setFormStatus("Submitted");
+    setSubmitCount((curValue) => curValue + 1);
+
+    // Since the fields all manage their own state, read form data from form's event
+    const formData = new FormData(event.currentTarget);
+    const fields = Object.fromEntries(formData.entries());
+
+    const formIsValid = Object.entries(fields).every(([fieldName, value]) => {
+      return !validate(fieldName as FieldName, value.toString());
+    });
+
+    if (formIsValid) {
+      event.currentTarget.reset();
+      console.log(`Fast Form Submitted`, fields);
+    }
   }
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        setFormStatus("Submitted");
-        setSubmitCount((curValue) => curValue + 1);
-      }}
-    >
+    <form noValidate onSubmit={handleSubmit}>
       {submitCount}
       {slowComponent}
+
       <FastInput
-        validate={required}
-        wasSubmitted={formStatus === "Submitted"}
-        name="one"
+        id="firstName"
+        label="First Name"
+        validate={validate}
+        formStatus={formStatus}
+        name="firstName"
       />
 
       <FastInput
-        validate={required}
-        wasSubmitted={formStatus === "Submitted"}
-        name="two"
+        id="email"
+        label="Email"
+        validate={validate}
+        formStatus={formStatus}
+        name="email"
       />
 
       <input type="submit" value="Submit" />
--- src/FastInput.tsx
+++ src/FastInput.tsx
@@ -1,34 +1,52 @@
 import { useState } from "react";
+import { FieldName, FormStatus, ValidateFunction } from "./FastForm";
+import "./FastInput.css";
 
 type FastInputProps = {
-  name: string;
-  wasSubmitted: boolean;
-  validate: (value: string) => string;
+  /** Field's name */
+  name: FieldName;
+
+  /** The form's submission status */
+  formStatus: FormStatus;
+
+  /** Input label */
+  label: string;
+
+  /** Input ID - Used to associated the label and input. */
+  id: string;
+
+  /** Function to execute to determine if the field is valid */
+  validate: ValidateFunction;
 };
 
 export default function FastInput({
   name,
-  wasSubmitted,
+  formStatus,
+  id,
+  label,
   validate,
 }: FastInputProps) {
   const [value, setValue] = useState("");
   const [touched, setTouched] = useState(false);
 
   // Derived state
-  // Note: This is fast, because it will only run when we're interacting
-  // with this specific field.
-  const error = validate(value);
+  // Note: This is efficient because it only runs when the user interacts with this field.
+  const error = validate(name, value);
 
   return (
     <div>
+      <label htmlFor={id}>{label}</label>
       <input
+        id={id}
         type="text"
         name={name}
         value={value}
         onBlur={() => setTouched(true)}
         onChange={(e) => setValue(e.target.value)}
       />
-      {error && (touched || wasSubmitted) && <p role="alert">{error}</p>}
+      {error && (touched || formStatus === "Submitted") && (
+        <p role="alert">{error}</p>
+      )}
     </div>
   );
 }
